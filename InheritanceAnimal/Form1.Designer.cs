138fe4c0354e7e61b05bd6d53fbdce000114cc9a
Kobe Delobelle <delobellekobe@gmail.com>
2021-01-19 11:28:07 +0100

    2e poging form design

--- InheritanceAnimal/Form1.Designer.cs
+++ InheritanceAnimal/Form1.Designer.cs
@@ -29,12 +29,195 @@ namespace InheritanceAnimal
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.rbCat = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbHuman = new System.Windows.Forms.RadioButton();
+            this.rbParrot = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPet = new System.Windows.Forms.Button();
+            this.btnFeed = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.btnSpeak = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // rbCat
+            // 
+            this.rbCat.AutoSize = true;
+            this.rbCat.Location = new System.Drawing.Point(6, 21);
+            this.rbCat.Name = "rbCat";
+            this.rbCat.Size = new System.Drawing.Size(50, 21);
+            this.rbCat.TabIndex = 0;
+            this.rbCat.TabStop = true;
+            this.rbCat.Text = "Cat";
+            this.rbCat.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbHuman);
+            this.groupBox1.Controls.Add(this.rbParrot);
+            this.groupBox1.Controls.Add(this.rbCat);
+            this.groupBox1.Location = new System.Drawing.Point(37, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(170, 121);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Animals";
+            // 
+            // rbHuman
+            // 
+            this.rbHuman.AutoSize = true;
+            this.rbHuman.Location = new System.Drawing.Point(6, 75);
+            this.rbHuman.Name = "rbHuman";
+            this.rbHuman.Size = new System.Drawing.Size(74, 21);
+            this.rbHuman.TabIndex = 2;
+            this.rbHuman.TabStop = true;
+            this.rbHuman.Text = "Human";
+            this.rbHuman.UseVisualStyleBackColor = true;
+            // 
+            // rbParrot
+            // 
+            this.rbParrot.AutoSize = true;
+            this.rbParrot.Location = new System.Drawing.Point(6, 48);
+            this.rbParrot.Name = "rbParrot";
+            this.rbParrot.Size = new System.Drawing.Size(68, 21);
+            this.rbParrot.TabIndex = 1;
+            this.rbParrot.TabStop = true;
+            this.rbParrot.Text = "Parrot";
+            this.rbParrot.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 182);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(395, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pick a sentence you would like to use in this super mega app:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(229, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(281, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Give your \"animal\" a name, and be original:";
+            // 
+            // btnPet
+            // 
+            this.btnPet.Location = new System.Drawing.Point(34, 338);
+            this.btnPet.Name = "btnPet";
+            this.btnPet.Size = new System.Drawing.Size(107, 64);
+            this.btnPet.TabIndex = 4;
+            this.btnPet.Text = "Pet";
+            this.btnPet.UseVisualStyleBackColor = true;
+            // 
+            // btnFeed
+            // 
+            this.btnFeed.Location = new System.Drawing.Point(289, 338);
+            this.btnFeed.Name = "btnFeed";
+            this.btnFeed.Size = new System.Drawing.Size(107, 64);
+            this.btnFeed.TabIndex = 5;
+            this.btnFeed.Text = "Feed";
+            this.btnFeed.UseVisualStyleBackColor = true;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(568, 338);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(107, 64);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(622, 44);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(107, 64);
+            this.btnCreate.TabIndex = 7;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            // 
+            // btnSpeak
+            // 
+            this.btnSpeak.Location = new System.Drawing.Point(289, 244);
+            this.btnSpeak.Name = "btnSpeak";
+            this.btnSpeak.Size = new System.Drawing.Size(107, 64);
+            this.btnSpeak.TabIndex = 8;
+            this.btnSpeak.Text = "Speak now or forever hold your breath:";
+            this.btnSpeak.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Hello",
+            "Good Morning?",
+            "How are you?",
+            "I\'m hungry, have food?",
+            "Boo!",
+            "Tell me something interesting pls?",
+            "If you add 10+12, how many fishes do you get?",
+            "What color is your hair?",
+            "What time is it?",
+            "Allright, allright, allright..."});
+            this.listBox1.Location = new System.Drawing.Point(34, 224);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(223, 84);
+            this.listBox1.TabIndex = 9;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(232, 65);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 22);
+            this.txtName.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnSpeak);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnFeed);
+            this.Controls.Add(this.btnPet);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RadioButton rbCat;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbHuman;
+        private System.Windows.Forms.RadioButton rbParrot;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPet;
+        private System.Windows.Forms.Button btnFeed;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Button btnSpeak;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtName;
     }
 }
