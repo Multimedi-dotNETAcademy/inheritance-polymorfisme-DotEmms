58b109762572bbb96f8324332e4d3998b60ae702
DotEmms <emmadanckaert@hotmail.com>
2021-01-21 10:24:53 +0100

    Start ex3

---
+++ InheritanceCilinder/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceCilinder
+{
+    class Point
+    {
+        private double _x;
+
+        public double X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+        private double _y;
+
+        public double Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+    }
+}
